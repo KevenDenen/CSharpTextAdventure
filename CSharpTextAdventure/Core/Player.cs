5db119ac4b21ff4bdcc3316bc16dc21cef15d878
Keven Denen <keven@compasscontact.net>
2019-10-11 11:40:42 -0600

    Added inv and inventory commands and the support for them

--- CSharpTextAdventure/Core/Player.cs
+++ CSharpTextAdventure/Core/Player.cs
@@ -1,4 +1,6 @@
 ﻿using CSharpTextAdventure.Interfaces;
+using System.Collections.Generic;
+using CSharpTextAdventure.Systems;
 
 namespace CSharpTextAdventure.Core
 {
@@ -6,7 +8,7 @@ namespace CSharpTextAdventure.Core
     {
         public Player(string article, string adjective, string noun, string description) : base(article, adjective, noun, description)
         {
-
+            Inventory = new List<GameObject>();
         }
         public int Strength { get; set; }
         public int Dexterity { get; set; }
@@ -15,6 +17,23 @@ namespace CSharpTextAdventure.Core
         public override string Look()
         {
             return "You look like you've always looked.";
+        }
+
+        public string PrintInventory()
+        {
+            string inventory = "You are currently carrying: ";
+
+            if (Inventory.Count == 0)
+            {
+                inventory += " nothing";
+            }
+            else
+            {
+                inventory += Inventory.OxfordCommaJoin();
+            }
+
+            inventory += ".";
+            return inventory;
         }
     }
 }
---
+++ CSharpTextAdventure/Systems/Helpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSharpTextAdventure.Core;
+
+namespace CSharpTextAdventure.Systems
+{
+    public static class Helpers
+    {
+        public static string OxfordCommaJoin(this IEnumerable<GameObject> collection)
+        {
+            if (collection == null)
+            {
+                return string.Empty;
+            }
+
+            if (!collection.Any())
+            {
+                return string.Empty;
+            }
+
+            var list = collection.ToList();
+
+            if (list.Count == 1)
+            {
+                return list.First().ToString();
+            }
+
+            if (list.Count == 2)
+            {
+                return $"{list.First().ToString()} and {list.Last().ToString()}";
+            }
+
+            var result = string.Join(", ", list.Take(list.Count - 1));
+            result += $", and {list.Last().ToString()}";
+
+            return result;
+        }
+    }
+}
